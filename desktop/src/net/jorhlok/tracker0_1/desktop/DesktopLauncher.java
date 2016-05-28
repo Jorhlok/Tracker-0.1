1120de0e295feb3f0201778df8a0c0fc435251be
Jorhlok <joshmbrown42@gmail.com>
2016-05-27 21:02:57 -0500

    Create new libGDX project.

---
+++ desktop/src/net/jorhlok/tracker0_1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.jorhlok.tracker0_1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.jorhlok.tracker0_1.tracker0_1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new tracker0_1(), config);
+	}
+}
